77336d2e647217027778412f682c80666800f24f
Yara Ghaben <yaraghaben14@gmail.com>
2025-01-18 21:46:13 +0200

    Initial commit

---
+++ BankManagementSys/src/main/java/com/example/BankManagementSys/BankManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankManagementSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementSysApplication.class, args);
+	}
+
+}
---
+++ BankManagementSys/src/test/java/com/example/BankManagementSys/BankManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankManagementSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
